bd90548ac348cb66dda5eed57fdbc2362155a006
Juan <pablobedoya3521@gmail.com>
2025-04-20 14:34:00 -0500

    Organizando el purchase entidad

--- frontend/src/main/java/com/project/frontend/purchasesSystem/models/Purchase.java
+++ frontend/src/main/java/com/project/frontend/purchasesSystem/models/Purchase.java
@@ -12,4 +12,56 @@ public class Purchase {
     private LocalDate date;
     private List<Product> products;
     private double total;
+
+    public Purchase() {
+    }
+
+    public Purchase(Client client, LocalTime hour, LocalDate date, List<Product> products, double total) {
+        this.client = client;
+        this.hour = hour;
+        this.date = date;
+        this.products = products;
+        this.total = total;
+    }
+
+    public Client getClient() {
+        return client;
+    }
+
+    public void setClient(Client client) {
+        this.client = client;
+    }
+
+    public LocalTime getHour() {
+        return hour;
+    }
+
+    public void setHour(LocalTime hour) {
+        this.hour = hour;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<Product> products) {
+        this.products = products;
+    }
+
+    public double getTotal() {
+        return total;
+    }
+
+    public void setTotal(double total) {
+        this.total = total;
+    }
+
 }
